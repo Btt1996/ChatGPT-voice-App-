f5d842e9914b50d9405761e603e7d7f2799044e8
ahmed taboubi <61942133+Btt1996@users.noreply.github.com>
2023-03-18 14:18:13 +0100

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT Voice Assistant',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+// Initialize speech-to-text and text-to-speech instances
+final stt.SpeechToText speech = stt.SpeechToText();
+final FlutterTts flutterTts = FlutterTts();
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isListening = false;
+  String _text = 'Press the button and start speaking';
+  double _confidence = 1.0;
+  TtsState ttsState = TtsState.stopped;
+
+  @override
+  void initState() {
+    super.initState();
+    _speak();
+  }
+
+  // Speech-to-text functionality
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await speech.initialize(
+          onStatus: (val) => print('onStatus: $val'),
+          onError: (val) => print('onError: $val'));
+      if (available) {
+        setState(() => _isListening = true);
+        speech.listen(
+            onResult: (val) => setState(() {
+                  _text = val.recognizedWords;
+                  if (val.hasConfidenceRating && val.confidence > 0) {
+                    _confidence = val.confidence;
+                  }
+                }));
+      }
+    } else {
+      setState(() => _isListening = false);
+      speech.stop();
+    }
+  }
+
+  // Text-to-speech functionality
+  Future<void> _speak() async {
+    await flutterTts.setVolume(1.0);
+    await flutterTts.setSpeechRate(0.5);
+    await flutterTts.setPitch(1.0);
+
+    if (_text != null && _text.isNotEmpty) {
+      var result = await flutterTts.speak(_text);
+      if (result == 1) setState(() => ttsState = TtsState.playing);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ChatGPT Voice Assistant'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _text,
+              style: TextStyle(fontSize: 32.0),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              'Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(
+              height: 32.0,
+            ),
+            FloatingActionButton(
+              onPressed: _listen,
+              child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+              backgroundColor: _isListening ? Colors.red : Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
